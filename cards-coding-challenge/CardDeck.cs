aec3c004c2b1388eaf38299abd60318ad300f442
Morgan West <mrw6863@rit.edu>
2023-02-07 15:07:58 -0500

    initial commit

---
+++ cards-coding-challenge/CardDeck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+/// <summary>
+/// Class <c>Cards</c> models a deck of poker cards.
+/// </summary>
+namespace CardClass
+{
+    /// <summary>
+    /// Enum <c>CardSuit</c> contains the potential suits of the cards.
+    /// </summary>
+    public enum CardSuit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    /// <summary>
+    /// Interface <c>Cards</c> stubbs out the desired functionality of the Cards class
+    /// </summary>
+    public interface ICards
+    {
+        /// <summary>
+        /// Method <c>GetCardSuit</c> gets the suit of a card
+        /// </summary>
+        CardSuit GetCardSuit();
+
+
+        /// <summary>
+        /// Method <c>GetCardNumber</c> gets the number of a card
+        /// </summary>
+        int GetCardNumber();
+    }
+
+    /// <summary>
+    /// Class <c>Cards</c> implments the desired functionality of the Cards class
+    /// </summary>
+    public class Card : ICards
+    {
+        private CardSuit Suit;
+        private int Number;
+        public Card(CardSuit suit, int number)
+        {
+            this.Suit = suit;
+            this.Number = number;
+        }
+
+
+        /// <summary>
+        /// Method <c>GetCardSuit</c> gets the suit of a card
+        /// </summary>
+        public CardSuit GetCardSuit()
+        {
+            return this.Suit;
+        }
+
+
+        /// <summary>
+        /// Method <c>GetCardNumber</c> gets the number of a card
+        /// </summary>
+        public int GetCardNumber()
+        {
+            return this.Number;
+        }
+
+        public static void Main()
+        {
+            Card cards = new Card(CardSuit.Hearts, 1);
+        }
+    }
+}
---
+++ cards-test-coding-challenge/CardsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CardClass;
+
+/// <summary>
+/// Namespace <c>CardsTests</c> contains the testing features of the Cards class.
+/// </summary>
+namespace CardsTests
+{
+    [TestClass]
+    public class CardTests
+    {
+
+        /// <summary>
+        /// Method <c>CreateCard</c> tests the creation of a one of hearts.
+        /// </summary>
+        [TestMethod]
+        public void CreateCard()
+        {
+            /// Create expected results and the test card
+            CardSuit expectedSuit = CardSuit.Hearts;
+            int expectedNumber = 1;
+            Card testCard = new Card(CardSuit.Hearts, 1);
+
+            /// Get the card suit and number
+            CardSuit actualSuit = testCard.GetCardSuit();
+            int actualNumber = testCard.GetCardNumber();
+
+            Assert.AreEqual(expectedSuit, actualSuit, "The card suit was not set as expected.");
+            Assert.AreEqual(expectedNumber, actualNumber, "The card number was not set as expected.");
+        }
+    }
+}
