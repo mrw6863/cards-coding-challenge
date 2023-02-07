ae93a45d63b93bf9d7bef212ec64cae4a871cdbb
Morgan West <mrw6863@rit.edu>
2023-02-07 17:05:31 -0500

    implemented deck functionality and tests

--- cards-coding-challenge/CardDeck.cs
+++ cards-coding-challenge/CardDeck.cs
@@ -63,9 +63,75 @@ namespace CardClass
             return this.Number;
         }
 
+        /// <summary>
+        /// Method <c>Main</c> creates an ace of hearts
+        /// </summary>
         public static void Main()
         {
-            Card cards = new Card(CardSuit.Hearts, 1);
+            Card cards = new Card(CardSuit.Diamonds, 2);
+        }
+    }
+
+    /// <summary>
+    /// Class <c>CardDeck</c> implments the desired functionality of the CardDeck class
+    /// </summary>
+    public class CardDeck
+    {
+        public List<Card> Cards;
+        public CardDeck? cardDeck;
+        public CardDeck()
+        {
+            Cards = new List<Card>();
+            var suits = Enum.GetValues(typeof(CardSuit));
+            /// for each potential card suit
+            foreach (CardSuit suit in suits)
+            {
+                /// iterate through the 13 potential cards
+                for (int i = 1; i <= 13; i++)
+                    /// add a new card with the suit and the next number
+                    Cards.Add(new Card(suit, i));
+            }
+        }
+
+        /// <summary>
+        /// Method <c>ShuffleCard</c> returns no value but results in the cards in the deck being randomly permuted 
+        /// </summary>
+        public void Shuffle()
+        {
+            /// initalize new instance of the random class
+            Random random = new Random();
+            /// for every possible card in the deck (52 cards)
+            for (int i = 0; i < 52; i++)
+            {
+                /// randomly select a number
+                int j = random.Next(0, 51);
+                /// set a temporary card to the current card
+                Card tempCard = Cards[i];
+                /// replace the current card with the randomly selected card
+                Cards[i] = Cards[j];
+                /// set the randomly selected card to the temporary card
+                Cards[j] = tempCard;
+            }
+        }
+
+        /// <summary>
+        /// Method <c>DealOneCard</c> return one card from the deck to the caller. If all cards have been dealt, no card is returned.
+        /// </summary>
+        /// <returns>One card from the deck to the caller</returns>
+        public Card? DealOneCard()
+        {
+            /// if there are any cards remaining in the deck
+            if (Cards.Any())
+            {
+                /// draw the first card
+                Card firstCard = Cards[0];
+                /// remove the first card
+                Cards.RemoveAt(0);
+                /// return the first card
+                return firstCard;
+            }
+            /// otherwise return null
+            return null;
         }
     }
 }
--- cards-test-coding-challenge/CardsTest.cs
+++ cards-test-coding-challenge/CardsTest.cs
@@ -11,22 +11,153 @@ namespace CardsTests
     {
 
         /// <summary>
-        /// Method <c>CreateCard</c> tests the creation of a one of hearts.
+        /// Test <c>CreateCardTest</c> tests the creation of a one of hearts.
         /// </summary>
         [TestMethod]
-        public void CreateCard()
+        public void CreateCardTest()
+        {
+            /// Create expected results and the test card
+            Card testCard = new Card(CardSuit.Hearts, 1);
+
+            Assert.IsNotNull(testCard);
+        }
+
+
+        /// <summary>
+        /// Test <c>GetCardSuitTest</c> tests getting a card suit
+        /// </summary>
+        [TestMethod]
+        public void GetCardSuitTest()
         {
             /// Create expected results and the test card
             CardSuit expectedSuit = CardSuit.Hearts;
+            Card testCard = new Card(CardSuit.Hearts, 1);
+
+            /// Get the card suit
+            CardSuit actualSuit = testCard.GetCardSuit();
+
+            Assert.AreEqual(expectedSuit, actualSuit, "The card suit was not set as expected.");
+        }
+
+
+        /// <summary>
+        /// Test <c>GetCardNumberTest</c> tests getting a card number
+        /// </summary>
+        [TestMethod]
+        public void GetCardNumberTest()
+        {
+            /// Create expected results and the test card
             int expectedNumber = 1;
             Card testCard = new Card(CardSuit.Hearts, 1);
 
-            /// Get the card suit and number
-            CardSuit actualSuit = testCard.GetCardSuit();
+            /// Get the card number
             int actualNumber = testCard.GetCardNumber();
 
-            Assert.AreEqual(expectedSuit, actualSuit, "The card suit was not set as expected.");
             Assert.AreEqual(expectedNumber, actualNumber, "The card number was not set as expected.");
+        }
+    }
+
+
+    [TestClass]
+    public class DeckTests
+    {
+        /// <summary>
+        /// Test <c>CreateDeckTest</c> tests the creation of a deck of cards to be null.
+        /// </summary>
+        [TestMethod]
+        public void CreateDeckTest()
+        {
+            /// Create test deck
+            CardDeck cardDeck = new CardDeck();
+
+            // assert the card deck is not null
+            Assert.IsNotNull(cardDeck);
+        }
+
+
+        /// <summary>
+        /// Test <c>ShuffleDeckTest</c> tests if a deck is shuffled
+        /// </summary>
+        [TestMethod]
+        public void ShuffleDeckTest()
+        {
+            /// Create expected results and the test deck
+            CardDeck cardDeck = new CardDeck();
+            CardDeck shuffledCardDeck = new CardDeck();
+
+            // shuffle the card deck
+            shuffledCardDeck.Shuffle();
+
+            // assert the shuffled card deck is not the same as the unshuffled card deck
+            Assert.AreNotEqual(cardDeck, shuffledCardDeck, "The shuffled and unshuffled card deck are the same");
+        }
+
+
+        /// <summary>
+        /// Test <c>DealOneCardUnshuffledTest</c> tests if one card is dealt and removed from the deck unshuffled
+        /// </summary>
+        [TestMethod]
+        public void DealOneCardUnshuffledTest()
+        {
+            /// Create expected results and the test deck
+            CardDeck cardDeck = new CardDeck();
+
+            // deal one card
+            Card? dealtCard = cardDeck.DealOneCard();
+
+            // assert the returned card is not null and that the deck does not contain the dealt card
+            Assert.IsNotNull(dealtCard);
+            Assert.IsFalse(cardDeck.Cards.Contains(dealtCard));
+        }
+
+
+        /// <summary>
+        /// Test <c>DealOneCardShuffledTest</c> tests if one card is dealt and removed from the deck shuffled
+        /// </summary>
+        [TestMethod]
+        public void DealOneCardShuffledTest()
+        {
+            /// Create expected results and the test deck
+            CardDeck cardDeck = new CardDeck();
+
+            /// shuffle the card deck
+            cardDeck.Shuffle();
+            Card? dealtCard = cardDeck.DealOneCard();
+
+            /// assert the returned card is not null and that the deck does not contain the dealt card
+            Assert.IsNotNull(dealtCard);
+            Assert.IsFalse(cardDeck.Cards.Contains(dealtCard));
+        }
+
+
+        /// <summary>
+        /// Test <c>DealOneCardShuffledTest</c> tests if one card is dealt and removed from the deck shuffled
+        /// </summary>
+        [TestMethod]
+        public void DealAllCards()
+        {
+            /// Create expected results and the test deck
+            CardDeck cardDeck = new CardDeck();
+            List<Card> cards = new List<Card>();
+
+            /// shuffle the card deck
+            cardDeck.Shuffle();
+            /// get the first card to test
+            Card firstCard = cardDeck.DealOneCard();
+            /// get the rest of the cards in the deck 
+            for (int i = 0; i < 51; i++)
+            {
+                Card? dealtCard = cardDeck.DealOneCard();
+                cards.Append(dealtCard);
+
+            }
+            /// get the last card after getting the whole deck (should be null)
+            object lastCard = cardDeck.DealOneCard();
+
+
+            /// assert the returned card is not null and that the deck does not contain the dealt card
+            Assert.IsNull(lastCard);
+            Assert.IsFalse(cardDeck.Cards.Contains(firstCard));
         }
     }
 }
